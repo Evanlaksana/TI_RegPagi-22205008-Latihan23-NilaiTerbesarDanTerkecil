e7073ea41a8e3dcb27765cd3907405e018ccb1c0
Evanlaksana <132762464+Evanlaksana@users.noreply.github.com>
2023-10-29 18:43:08 +0700

    Add files via upload

---
+++ Latihan23/src/latihan23/Latihan23.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package latihan23;
+import java.util.Scanner;
+
+public class Latihan23 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+        System.out.println("=====Program Nilai Terbesar dan Terkecil Nilai Mahasiswa=====");
+        System.out.print("Masukkan Nama Petugas : ");
+        String namaPetugas = scanner.nextLine();
+
+
+        System.out.print("Masukkan Banyaknya Nilai Mahasiswa : ");
+        int banyaknyaNilaiMahasiswa = scanner.nextInt();
+
+        int[] nilaiMahasiswa = new int[banyaknyaNilaiMahasiswa];
+        for (int i = 0; i < banyaknyaNilaiMahasiswa; i++) {
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMahasiswa[i] = scanner.nextInt();
+        }
+        int nilaiTerbesar = Integer.MIN_VALUE;
+        int nilaiTerkecil = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < banyaknyaNilaiMahasiswa; i++) {
+            if (nilaiMahasiswa[i] > nilaiTerbesar) {
+                nilaiTerbesar = nilaiMahasiswa[i];
+            }
+            if (nilaiMahasiswa[i] < nilaiTerkecil) {
+                nilaiTerkecil = nilaiMahasiswa[i];
+            }
+        }
+        
+        System.out.println("\n");
+        System.out.println("====Hasil Nilai Mahasiswa====");
+        System.out.println("\n");
+        System.out.println("Nilai Terbesar: " + nilaiTerbesar);
+        System.out.println("Nilai Terkecil: " + nilaiTerkecil);
+        System.out.println("\n");
+        System.out.println("Petugas: " + namaPetugas);
+
+        scanner.close();
+         }
+        }
+
+       
+    
+    
+
